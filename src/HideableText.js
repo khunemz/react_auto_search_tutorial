00ca17b8d17f19d0f50bade253c2bf992b0303f2
Anongnat <anongnat@Anongnats-MacBook-Pro.local>
2020-03-07 22:34:29 +0700

    first commit

---
+++ src/HideableText.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+export default class HideableText extends Component {
+
+    constructor (props) {
+        super(props)
+
+        this.state = {
+            isHidden: false,
+        }
+    }
+
+    toggleIsHidden() {
+        this.setState((currentState) => ({
+            isHidden: !currentState.isHidden  
+        }));
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={()=>this.toggleIsHidden()}>Toggle</button>
+                {this.state.isHidden && this.props.text}
+            </div>
+        )
+    }
+}
